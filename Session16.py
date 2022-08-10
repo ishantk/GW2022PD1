8bd4dce4c3bb1ea70b6cbe11064718c9060d5a05
ishantk <er.ishant@gmail.com>
2022-08-10 11:11:23 +0530

    MySQL DB Operations

--- Session16.py
+++ Session16.py
@@ -41,15 +41,19 @@ class DBHelper:
         self.connection.commit()
         print("SQL QUERY EXECUTED :)")
 
-    def read(self):
-        pass
+    def read(self, sql):
+        self.cursor.execute(sql)
+        rows = self.cursor.fetchall()
+        for row in rows:
+            print(row)
+            # print(row[1], row[2])
 
 
 class HealthLogger:
 
     db_helper = DBHelper()
 
-    def __init__(self):
+    def save_log(self):
         self.id = None
         self.phone = input("Enter Phone Number: ")
         self.weight = int(input("Enter Weight: "))
@@ -60,14 +64,59 @@ class HealthLogger:
         idx = dt.rindex(".")
         self.log_time_stamp = dt[0: idx]
 
-    def show(self):
-        print(vars(self))
-
-    def save_log(self):
-        sql = "insert into HealthLogger values(null, '{phone}', {weight}, {bp_high}, {bp_low}, {sugar}, '{log_time_stamp}')".format_map(vars(self))
+        sql = "insert into HealthLogger values(null, '{phone}', " \
+              "{weight}, {bp_high}, {bp_low}, {sugar}, " \
+              "'{log_time_stamp}')".format_map(vars(self))
         print(sql)
-        self.db_helper.write(sql)
 
+        save = input("Do you wish to Save the Record (yes/no)")
+        if save == "yes":
+            self.db_helper.write(sql)
+
+
+    def update_log(self):
+        self.id = int(input("Enter Log ID: "))
+        self.phone = input("Enter Phone Number: ")
+        self.weight = int(input("Enter Weight: "))
+        self.bp_high = int(input("Enter BP High: "))
+        self.bp_low = int(input("Enter BP Low: "))
+        self.sugar = int(input("Enter Diabetes: "))
+        dt = str(datetime.datetime.today())
+        idx = dt.rindex(".")
+        self.log_time_stamp = dt[0: idx]
+
+        sql = "update HealthLogger set" \
+              " phone = '{phone}', " \
+              "weight = {weight}, bp_high = {bp_high}, bp_low = {bp_low}, " \
+              "sugar = {sugar}, log_time_stamp = '{log_time_stamp}' " \
+              "where id = {id}".format_map(
+            vars(self))
+
+        print(sql)
+
+        update = input("Do you wish to Update the Record (yes/no)")
+        if update == "yes":
+            self.db_helper.write(sql)
+
+    def delete_log(self):
+        self.id = int(input("Enter Log ID: "))
+
+        sql = "delete from HealthLogger where id = {id}".format_map(
+            vars(self))
+
+        print(sql)
+
+        delete = input("Do you wish to Delete the Record (yes/no)")
+        if delete == "yes":
+            self.db_helper.write(sql)
+
+    def get_all_logs(self):
+        sql = "select * from HealthLogger"
+        self.db_helper.read(sql)
+
+    def get_logs_by_phone(self, phone):
+        sql = "select * from HealthLogger where phone = {}".format(phone)
+        self.db_helper.read(sql)
 
 print("~~~~~~~~~~~~~~~~~~~~")
 print("WELCOME")
@@ -77,14 +126,45 @@ print("~~~~~~~~~~~~~~~~~~~~")
 
 while True:
 
-    print("Create a New Health Log")
+    print("1: Insert a New Health Log")
+    print("2: Update an Existing Health Log")
+    print("3: Delete an Existing Health Log")
+    print("4: Get All Health Logs")
+    print("5: Get All Health Logs By Phone")
 
-    logger = HealthLogger()
-    logger.show()
+    option = int(input("Enter Your Option: "))
 
-    save = input("Do you wish to Save the Record (yes/no)")
-    if save == "yes":
+    if option == 1:
+        print("Create a New Health Log")
+        logger = HealthLogger()
         logger.save_log()
+
+    elif option == 2:
+        print("Update Existing Health Log")
+
+        logger = HealthLogger()
+        logger.update_log()
+
+    elif option == 3:
+        print("Delete Existing Health Log")
+
+        logger = HealthLogger()
+        logger.delete_log()
+
+    elif option == 4:
+        print("Get All Health Logs")
+
+        logger = HealthLogger()
+        logger.get_all_logs()
+
+    elif option == 5:
+        print("Get All Health Logs by Phone")
+
+        logger = HealthLogger()
+        logger.get_logs_by_phone(input("Enter Phone Number: "))
+
+    else:
+        print("Invalid Option")
 
     choice = input("Would You Like to add another Health Log... (yes/no)")
     if choice == "no":
